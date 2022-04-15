580f67a671e44fc76b86941b0e4742f0ae2a5bc2
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-04-16 04:52:42 +0600

    Navbar added

--- client/origin-mtm/src/app/app.module.ts
+++ client/origin-mtm/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    
   ],
   imports: [
     BrowserModule,
