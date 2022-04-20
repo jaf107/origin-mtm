0595b8ed16db9eccc31f9bf282072c49b60c9291
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-04-21 01:03:03 +0600

    css

--- client/src/app/components/user-profile/user-profile.component.ts
+++ client/src/app/components/user-profile/user-profile.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from './../../shared/auth.service';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss'],
+  styleUrls: ['./user-profile.component.css'],
 })
 
 export class UserProfileComponent implements OnInit {
