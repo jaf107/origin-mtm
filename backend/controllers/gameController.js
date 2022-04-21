78062e704cff8f3f55b8d6eeb39f91eb433bbdc5
Tasmia Zerin <ttaosnmniya.6638.cross@gmail.com>
2022-04-21 22:04:23 +0600

    fixed carousel

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,9 +1,13 @@
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const games = require("../models/games")
 
-exports.getGames = async((req, res) => {
- const db = await games.find();
- res.status(200).json({
-     success,
-     db
- })  
-})
+
+exports.getGames = catchAsyncErrors(async (req, res, next) => {
+
+    const db = await games.find();
+  
+    res.status(200).json({
+        db
+    }) 
+  });
+  
